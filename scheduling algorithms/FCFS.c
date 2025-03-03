24636447bdcd4336db1a31ddff098143520a66f7
XumoVoidHD <vedanshsahay05@gmail.com>
2025-03-04 00:05:55 +0530

    FCFS corrected

--- scheduling algorithms/FCFS.c
+++ scheduling algorithms/FCFS.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 #define MAX 100
 
 struct Process
 {
-    float pid;
-    float arrival;
-    float burst;
-    float waiting;
-    float turnaround;
-    float completion;
+    int pid;
+    int arrival;
+    int burst;
+    int waiting;
+    int turnaround;
+    int completion;
 };
 
+// Function to sort processes by arrival time (using Bubble Sort)
 void sort(struct Process p[], int n)
 {
     struct Process temp;
@@ -28,28 +30,33 @@ void sort(struct Process p[], int n)
     }
 }
 
+// Function to implement First Come First Serve (FCFS) Scheduling
 void fcfs(struct Process p[], int n)
 {
-    sort(p, n);
-    int time = 0;
+    sort(p, n); // Sort processes by arrival time
+    int time = 0; // Track current time
 
     for (int i = 0; i < n; i++)
     {
+        // If CPU is idle, move time forward
         if (time < p[i].arrival)
         {
             time = p[i].arrival;
         }
 
+        // Calculate process completion time
         p[i].completion = time + p[i].burst;
         p[i].turnaround = p[i].completion - p[i].arrival;
         p[i].waiting = p[i].turnaround - p[i].burst;
-        time = p[i].completion;
+        time = p[i].completion; // Update current time
     }
 
-    printf("PID\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n");
+    // Display results
+    printf("\nPID\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n");
     for (int i = 0; i < n; i++)
     {
-        printf("%d\t%d\t%d\t%d\t\t%d\t\t%d\n", p[i].pid, p[i].arrival, p[i].burst,
+        printf("%d\t%d\t%d\t%d\t\t%d\t\t%d\n", 
+               p[i].pid, p[i].arrival, p[i].burst, 
                p[i].completion, p[i].turnaround, p[i].waiting);
     }
 }
@@ -58,16 +65,29 @@ int main()
 {
     int n;
 
+    // Get number of processes
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
+    // Check if n exceeds MAX
+    if (n > MAX)
+    {
+        printf("Error: Maximum number of processes is %d\n", MAX);
+        return 1;
+    }
+
     struct Process p[MAX];
 
-    printf("Enter Process ID, Arrival Time and Burst Time:\n");
+    // Get process details
+    printf("Enter Process ID, Arrival Time, and Burst Time:\n");
     for (int i = 0; i < n; i++)
     {
+        printf("Process %d: ", i + 1);
         scanf("%d %d %d", &p[i].pid, &p[i].arrival, &p[i].burst);
     }
+
+    // Run FCFS Scheduling
+    fcfs(p, n);   
 
     return 0;
 }
