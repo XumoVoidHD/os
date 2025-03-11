bbb74bef6c20ec731603c88384f4309d4a3a60eb
XumoVoidHD <vedanshsahay05@gmail.com>
2025-03-11 06:04:37 +0530

    correction

--- pat_1/Q1.sh
+++ pat_1/Q1.sh
@@ -3,17 +3,23 @@ read -a array
 new_array=()
 
 for ele in "${array[@]}"; do
-        if (( ele % 2 == 0 )); then
-                new_array+=("$ele")
-        fi
-done
-
-for ele in "${array[@]}"; do
         if (( ele % 2 != 0 )); then
                 new_array+=("$ele")
         fi
 done
 
-echo "Original Array: "${array[@]}"
-echo "New Array: "${new_array[@]}"
+for ele in "${array[@]}"; do
+        if (( ele % 2 == 0 )); then
+                new_array+=("$ele")
+        fi
+done
+
+echo "Original Array: ${array[@]}"
+echo "New Array: ${new_array[@]}"
 echo "Length of array: ${#array[@]}"
+
+for ((i=0; i<${#array[@]}; i++ )); do
+        if (( array[i] == new_array[i] )); then
+                echo "${array[i]}"
+        fi
+done
